66abecd34eb2e771d2e2c01b5036306d370bf149
Msbee502 <b.hoogesteyn@gmail.com>
2024-11-20 13:58:08 +0100

    Changing css

--- Assignment7-JS-React/src/components/gallery.jsx
+++ Assignment7-JS-React/src/components/gallery.jsx
@@ -13,32 +13,32 @@ return (
         <h1>Our products and services</h1>
         <p></p>
       
-        <div className='firstPic'>
-            <img src={Glassbottle} alt="Glassbottle" className='glassBottle'/>
+        <div className='galleryContainer'>
+            <img src={Glassbottle} alt="Glassbottle" className='galleryPic'/>
             <h3>Herb Tinktures</h3>
             <p>From $8.00</p>
         </div>
 
-        <div className='secondPic'>
-            <img src={Healthydrink} alt="Glass with healthy drink" className='healthyDrink'/>
+        <div className='galleryContainer'>
+            <img src={Healthydrink} alt="Glass with healthy drink" className='galleryPic'/>
             <h3>Smoothie Powders</h3>
             <p>From $8.99</p>
          </div>
       
-        <div className='thirdpic'>
-            <img src={Healthyfood} alt="An old book on a table" className='healthyFood' />
+        <div className= 'galleryContainer'>
+            <img src={Healthyfood} alt="An old book on a table" className='galleryPic' />
             <h3>Organic Produce</h3>
             <p>From $3.99</p>
         </div>
 
-        <div className='fifthpic'>
-            <img src={Teapot} alt="Tea in pot" className='teaPot'/>
+        <div className='galleryContainer'>
+            <img src={Teapot} alt="Tea in pot" className='galleryPic'/>
             <h3>Our very own teablends!</h3>
             <p>From $5.99</p>
         </div>
 
-        <div className='fourthpic'>
-            <img src={Workout} alt="An old book on a table" className='workOut' />
+        <div className='galleryContainer'>
+            <img src={Workout} alt="An old book on a table" className='galleryPic' />
             <h3>Health-Coach</h3>
             <p>Contact us for more info</p>
         </div>       
