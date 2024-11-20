5e652b00157fe5e773cb7f70d8f8da771ca438a4
Msbee502 <b.hoogesteyn@gmail.com>
2024-11-20 11:38:51 +0100

    Adding css

--- Assignment7-JS-React/src/components/gallery.jsx
+++ Assignment7-JS-React/src/components/gallery.jsx
@@ -4,43 +4,44 @@ import Healthyfood from '../assets/Healthyfood.png';
 import Workout from '../assets/Workout.png';
 import Teapot from '../assets/Teapot.png';
 
+
 function Gallery (){
 
 return (
 
     <div className='Gallery'>
-        <h1>Glass Gallery</h1>
-        <p>Some text</p>
+        <h1>Our products and services</h1>
+        <p></p>
       
-        <div className='firstpic'>
-            <img src={Glassbottle} alt="Glassbottle" width={150} height={150}/>
-            <h3>Some text</h3>
-            <p>Lorem ipsum dolor sit!</p>
+        <div className='firstPic'>
+            <img src={Glassbottle} alt="Glassbottle" className='glassBottle'/>
+            <h3>Herb Tinktures</h3>
+            <p>From $8.00</p>
         </div>
 
-        <div className='secondpic'>
-            <img src={Healthydrink} alt="Glass with healthy drink" width={150} height={150} />
-            <h3>Some text</h3>
-            <p>Lorem ipsum dolor sit!</p>
+        <div className='secondPic'>
+            <img src={Healthydrink} alt="Glass with healthy drink" className='healthyDrink'/>
+            <h3>Smoothie Powders</h3>
+            <p>From $8.99</p>
          </div>
       
         <div className='thirdpic'>
-            <img src={Healthyfood} alt="An old book on a table" width={150} height={150} />
-            <h3>Some text</h3>
-            <p>Lorem ipsum dolor sit!</p>
-        </div>
-
-        <div className='fourthpic'>
-            <img src={Workout} alt="An old book on a table" width={150} height={150} />
-            <h3>Some text</h3>
-            <p>Lorem ipsum dolor sit!</p>
+            <img src={Healthyfood} alt="An old book on a table" className='healthyFood' />
+            <h3>Organic Produce</h3>
+            <p>From $3.99</p>
         </div>
 
         <div className='fifthpic'>
-            <img src={Teapot} alt="Tea in pot" width={150} height={150} />
-            <h3>Some text</h3>
-            <p>Lorem ipsum dolor sit!</p>
+            <img src={Teapot} alt="Tea in pot" className='teaPot'/>
+            <h3>Our very own teablends!</h3>
+            <p>From $5.99</p>
         </div>
+
+        <div className='fourthpic'>
+            <img src={Workout} alt="An old book on a table" className='workOut' />
+            <h3>Health-Coach</h3>
+            <p>Contact us for more info</p>
+        </div>       
 
         </div>
 
