55f3b2fb14bbc882946b56af8c5d15ce80aef211
Msbee502 <b.hoogesteyn@gmail.com>
2025-02-05 09:58:30 +0100

    .

--- Assignment7-JS-React/src/App.jsx
+++ Assignment7-JS-React/src/App.jsx
@@ -1,4 +1,4 @@
-import Gallery from './components/gallery.jsx';
+import Gallery from './pages/gallery.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home.jsx';
 import About from './pages/about.jsx';
@@ -7,14 +7,13 @@ import About from './pages/about.jsx';
 function App () {
   return (
     <div>
-      <BrowserRouter>
+   <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Gallery />} />
       </Routes>
       </BrowserRouter>
- 
 
       {/*... */}
     </div>
